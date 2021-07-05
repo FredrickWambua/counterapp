e90f26fd83d319ccd47c139b4d986e57cfb624d1
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-07-05 13:28:09 +0300

    increment function created, count variable assigned

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-// document.getElementById("count-el").innerText = 5;
+// document.getElementById("count-el").innerText = console.log(count);
+// initializing a variable
 let count = 0;
 
-console.log(count)
+// assingning a variable to the HTML id
+countEl = document.getElementById("count-el")
+console.log(countEl)
+console.log(countEl)
+
+// increment function
+function increment(){
+    count = count +=1;
+    console.log(count)
+};
